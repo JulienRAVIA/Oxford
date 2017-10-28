187d171154a8f81becd5d89c1f3a670df337d7e9
Julien RAVIA <jrgfawkes@gmail.com>
2017-10-29 01:10:08 +0200

    Modification et ajout de routes :rocket:

--- public/index.php
+++ public/index.php
@@ -5,8 +5,10 @@ include_once '../bootstrap.php';
 $router = new AltoRouter();
 $router->setBasePath('/Oxford/public');
 
-$router->map('GET','/', 'App\\HomeController@index', 'home');
-$router->map('GET','/test', 'App\\HomeController@test', 'test');
+$router->map('GET','/', 'App\\HomeController@index', 'index');
+$router->map('GET','/home', 'App\\HomeController@index', 'home');
+$router->map('GET','/users', 'App\\UsersController@index', 'users');
+$router->map('GET','/user/[i:id]', 'App\\HomeController@showUser', 'user');
 
 $match = $router->match();
 
@@ -16,7 +18,11 @@ if (!$match) {
     list($controller, $action) = explode('@', $match['target']);
     $controller = new $controller;
     if (is_callable(array($controller, $action))) {
-        call_user_func_array(array($controller, $action), array($match['params']));
+        try {
+            call_user_func_array(array($controller, $action), array($match['params']));
+        } catch (Exception $e) {
+            App\View::make('error.twig', array('error' => $e->getMessage()));
+        }
     } else {
         echo 'Error: can not call ' . get_class($controller) . '@' . $action;
         // here your routes are wrong.
